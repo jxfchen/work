38848a63cbf7c93063072043157888890a39f3f3
王晨 <jxfchen_123@163.com>
2019-10-15 16:25:34 +0800

    first commit

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,82 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    customItem: '全部'
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      var list={
+        address_img:"/images/address.png",
+        region: ['山东省', '济南市', '全部'],
+        arrow_img:"/images/arrow.png",
+        background: ["/images/timg.jpg", "/images/timg.jpg", "/images/timg.jpg"],
+        background1: "/images/timg.jpg",
+      }
+      this.setData(list);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
