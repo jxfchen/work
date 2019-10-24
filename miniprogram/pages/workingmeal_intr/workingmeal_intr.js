2fa188ac2539690d98d86a7dbda876ea32e8036e
王晨 <jxfchen_123@163.com>
2019-10-24 21:16:09 +0800

    推客页面修改加 授权

--- miniprogram/pages/workingmeal_intr/workingmeal_intr.js
+++ miniprogram/pages/workingmeal_intr/workingmeal_intr.js
@@ -4,7 +4,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    background: ["/images/banner.png", "/images/banner.png", "/images/banner.png", "/images/banner.png", "/images/banner.png"],
   },
 
   /**
