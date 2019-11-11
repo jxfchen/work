432bf0eb207d8b18eb514b476826b10aa8d5df37
a8132 <15117983688@163.com>
2019-11-11 10:56:10 +0800

    2019-11-11 zhang

--- miniprogram/pages/authorized_login/authorized_login.js
+++ miniprogram/pages/authorized_login/authorized_login.js
@@ -3,34 +3,48 @@
 //获取应用实例
 const app = getApp()
 // 引入接口
-var e = require("../../utils/http.js");
+var c = require("../../utils/http.js");
 
 Page({
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   bindGetUserInfo(e) {
-    var that = this;
-    //获取用户信息
-    wx.getUserInfo({
-      success: function (res) {
-        console.log(res); //得到用户信息
-        wx.getSetting({
+    //查看是否授权
+    wx.getSetting({
+      success(res) {
+        wx.getUserInfo({
           success(res) {
-            if (res.authSetting['scope.userInfo']) {
-              var openid = wx.getStorageSync('openid')
-              console.log(openid)
+            console.log(res.userInfo)
+            wx.setStorage({
+              key: 'avatarurl',
+              data: res.userInfo.avatarUrl
+            })
+            wx.setStorage({
+              key: 'nickname',
+              data: res.userInfo.nickName
+            })
+            c.request("wechat/setUserinfo", {
+              openid: wx.getStorageSync('openid'),
+              nickname: res.userInfo.nickName,
+              gender: res.userInfo.gender,
+              avatarurl: res.userInfo.avatarUrl,
+              city: res.userInfo.city,
+              province: res.userInfo.province,
+              country: res.userInfo.country,
+              language: res.userInfo.language,
+            }, function (res) {
               wx.switchTab({
                 url: '../index/index',
               })
+              console.log(res)
+            }, function () {
               wx.showToast({
-                title: '授权成功',
+                title: '加载数据失败',
               })
-            } else {
-              wx.showToast({
-                title: '请您授权后使用',
-              })
+              console.log('shibai')
             }
+            )
           }
         })
       }
