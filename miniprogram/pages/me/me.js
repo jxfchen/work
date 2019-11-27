f20c76d85ed138305bc4802c623f3257abf6dceb
王晨 <jxfchen_123@163.com>
2019-11-27 19:20:05 +0800

    认证页面功能数据实现

--- miniprogram/pages/me/me.js
+++ miniprogram/pages/me/me.js
@@ -15,18 +15,6 @@ Page({
         wx.setClipboardData({
             data: self.data.list.user_info.invite_code,
             success: function (res) {
-                // self.setData({copyTip:true}),
-                // wx.showModal({
-                //     title: '提示',
-                //     content: '复制成功',
-                //     success: function (res) {
-                //         if (res.confirm) {
-                //             console.log('确定')
-                //         } else if (res.cancel) {
-                //             console.log('取消')
-                //         }
-                //     }
-                // })
             }
         });
     },
