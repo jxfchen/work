e8390fc1a87fa881d78f76d18e191c519f0de226
chee <723875993@qq.com>
2019-12-01 00:17:59 +0800

    推单列表优化

--- miniprogram/pages/sheetlist/sheetlist.js
+++ miniprogram/pages/sheetlist/sheetlist.js
@@ -1,5 +1,6 @@
 var app = getApp();
 var c = require("../../utils/http.js");
+var baseUrl = require("../../utils/config.js");
 Page({
   data: {
     // 左侧点击类样式
@@ -10,12 +11,12 @@ Page({
     list: [],
     curNav: 0,
     subclass: [],
+    lid: 0
   },
   onLoad: function (options) {
-    // wx.showToast({
-    //   icon: 'none',
-    //   title: "成功",
-    // })
+    this.getRestaurantList();
+  },
+  getRestaurantList: function () {
     var that = this
     c.request("restaurant/getRestaurantType", {
 
@@ -23,11 +24,11 @@ Page({
       console.log(res.info)
       that.setData({
         list: res.info,
-        lid: res.info[0].id
+        lid: that.data.lid ? that.data.lid : res.info[0].id
       })
       wx.request({
-        url: 'https://www.infinitybuild.cn/api.php/restaurant/getRestaurantList',
-        data:{
+        url: baseUrl.config.api_base_url + 'restaurant/getRestaurantList',
+        data: {
           type: that.data.lid,
           page: 1,
           size: 20,
@@ -35,12 +36,12 @@ Page({
         success: function (res) {
           console.log(res.data.infos)
           that.setData({
-            lt: res.data.infos
+            lt: res.data.infos ? res.data.infos : []
           })
         }
       })
     }, function () {
-      
+
     })
   },
   onShow: function () {
@@ -56,26 +57,25 @@ Page({
     var that = this
     var index = e.currentTarget.dataset.index
     var pid = e.currentTarget.dataset.id
-    console.log(pid)
     app.pid = pid
     this.setData({
       scrollTopId: index,
       // 左侧点击类样式
       curNav: index,
       toView: index,
-      vid: pid
+      vid: pid,
+      lid: pid
     })
-    console.log(that.data.vid)
     wx.request({
-      url: 'https://www.teckwrap.cn/api.php/index/getCateByPid',
+      url: baseUrl.config.api_base_url + 'index/getCateByPid',
       data: {
         pid: that.data.vid
       },
       success: function (res) {
-        console.log(res.data.info)
         that.setData({
           subclass: res.data.info
         })
+        that.getRestaurantList();
       }
     })
   },
