1da23cd829317d8ad61a6f69b46f5c7bcf44644a
a8132 <15117983688@163.com>
2019-12-06 16:15:53 +0800

    test

--- miniprogram/pages/a/a.js
+++ miniprogram/pages/a/a.js
@@ -1,32 +1,8 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
   },
   onLoad: function (options) {
-    var that = this;
-    var scene = decodeURIComponent(options.scene)
-    wx.request({
-      url: 'https://www.infinitybuild.cn?access_token=',
-      data: {
-        scene: '000',//邀请码
-        page: ""
-      },
-      success(res) {
-        console.log(res)
-        var src2 = wx.arrayBufferToBase64(res.data);  //对数据进行转换操作
-        that.setData({
-          src2
-        })
-      },
-      fail(e) {
-        console.log(e)
-      }
-    })
-
-
+    
   },
 
 })
