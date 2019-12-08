29d4ea69e8766ac0c4a2120c63bc3ceea2e41b17
王晨 <jxfchen_123@163.com>
2019-12-08 13:35:46 +0800

    12.8前端问题修改

--- miniprogram/pages/msg/msg.js
+++ miniprogram/pages/msg/msg.js
@@ -10,7 +10,9 @@ Page({
     hasMore: true,
     lists: []
   },
-
+    click1tap:function(){
+        
+    },
   /**
    * 生命周期函数--监听页面加载
    */
--- miniprogram/pages/msg_content/msg_content.js
+++ miniprogram/pages/msg_content/msg_content.js
@@ -13,7 +13,39 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+        this.getMsgList();
+    },
+    getMsgList: function (isNext = false) {
+        var _this = this, openid = wx.getStorageSync('openid');
+        c.request("message/getMessageList", {
+            openid: openid,
+            page: _this.data.pageNo,
+            size: 10
+        }, function (res) {
+            var list_str = JSON.stringify(res.info);
+            var list = JSON.parse(list_str);
+            if (res.info == null || res.info.length == 0) {
+                _this.setData({
+                    hasMore: false,
+                    lists: []
+                });
+                return false;
+            }
+            if (isPage) {
+                //下一页的数据拼接在原有数据后面
+                list = _this.data.list.concat(list);
+                _this.setData({
+                    lists: list
+                });
+            } else {
+                //第一页数据直接赋值
+                _this.setData({
+                    lists: list
+                });
+            }
+        }, function () {
+            console.log('fail');
+        })
   },
 
   /**
